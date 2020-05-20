a2c096c0b075061819399a02d90ef1b9c5f6d996
Leandro Moreira <leandro.moreira@zoop.com.br>
2020-05-20 16:24:07 -0300

    Setting the environment at the end of the resource name and changing the configuration of the application tag.

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 locals {
-  id             = lower(join(var.delimiter, compact(concat(list(var.application, var.environment, var.namespace), var.attributes))))
-  application    = lower(format("%v", var.application))
+  id             = lower(join(var.delimiter, compact(concat(list(var.application, var.namespace, var.environment), var.attributes))))
+  application    = lower(join(var.delimiter, compact(concat(list(var.application, var.namespace), var.attributes))))
   service        = lower(format("%v", var.service))
   environment    = lower(format("%v", var.environment))
   business_owner = lower(format("%v", var.business_owner))
